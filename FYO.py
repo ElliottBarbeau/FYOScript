973e699e45da02096261a787b9b3fa20b41b1b53
ElliottBarbeau <31967133+ElliottBarbeau@users.noreply.github.com>
2018-07-08 22:07:45 -0400

    Separation methods
    Added all methods that separate values from multiple choice questions.

--- FYO.py
+++ FYO.py
@@ -4,12 +4,10 @@ def readcsv(fileName):
     with open(fileName, encoding="Latin-1") as csvfile:
         reader = csv.DictReader(csvfile)
         a = []
-        lineCount = 0
         next(reader)
         
         for line in reader:
-            a.append(line)
-            lineCount=+1            
+            a.append(line)         
     return(a)
 
 def streamSep(fileName):
@@ -20,6 +18,74 @@ def streamSep(fileName):
         
         for line in reader:
             temp.append(line['stream'])
+        
+        for i in temp:
+            if ";" in i:
+                final.append(i.split(";"))
+            else:
+                final.append(i)
+
+    return(final)
+
+def internshipSep(fileName):
+    with open(fileName, encoding="Latin-1") as csvfile:
+        reader = csv.DictReader(csvfile)
+        final = []
+        temp = []
+        
+        for line in reader:
+            temp.append(line['internship'])
+        
+        for i in temp:
+            if ";" in i:
+                final.append(i.split(";"))
+            else:
+                final.append(i)
+
+    return(final)
+
+def clubsSep(fileName):
+    with open(fileName, encoding="Latin-1") as csvfile:
+        reader = csv.DictReader(csvfile)
+        final = []
+        temp = []
+        
+        for line in reader:
+            temp.append(line['clubs'])
+        
+        for i in temp:
+            if ";" in i:
+                final.append(i.split(";"))
+            else:
+                final.append(i)
+
+    return(final)
+
+def skillsSep(fileName):
+    with open(fileName, encoding="Latin-1") as csvfile:
+        reader = csv.DictReader(csvfile)
+        final = []
+        temp = []
+        
+        for line in reader:
+            temp.append(line['skills'])
+        
+        for i in temp:
+            if ";" in i:
+                final.append(i.split(";"))
+            else:
+                final.append(i)
+
+    return(final)
+
+def profSkillsSep(fileName):
+    with open(fileName, encoding="Latin-1") as csvfile:
+        reader = csv.DictReader(csvfile)
+        final = []
+        temp = []
+        
+        for line in reader:
+            temp.append(line['profSkills'])
         
         for i in temp:
             if ";" in i:
