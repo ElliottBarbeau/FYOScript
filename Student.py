55496cb9386ce3dfaffa192e79c8119ff2ae744c
Declan McMullen <decomcmullen@gmail.com>
2018-08-10 09:39:41 -0400

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,3 +1,13 @@
 class Student:
 	"""Class for student"""
-	def __init__ (self, name, answers)
+	def __init__ (self, name, answers):
+		self.name = name
+		self.answers = answers
+	
+	def getAnswer(self, index):
+		return self.answers[i]
+	def getAcademicAnswers:
+	
+##It was decided that an object was not needed. Instead, we'll be simply using constants for stuff such as getting academic answers and whatnot
+
+	
