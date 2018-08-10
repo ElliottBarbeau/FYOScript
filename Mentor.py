b97d00cf8002c36eb962397515e0da30856a2827
Declan McMullen <decomcmullen@gmail.com>
2018-08-10 14:18:01 -0400

    Adding match and unmatch methods and variables
    Adding variables for second student

--- Mentor.py
+++ Mentor.py
@@ -8,7 +8,21 @@ class Mentor:
 		self.name = name
 		self.answers = answers
 		self.student = None
-		self.hasStudent = false
+		self.hasStudent = False
 		self.matchScore = 0
+		self.secondStudent = None
+		self.hasSecondStudent = False
 		
+	def unmatch(self):
+		tempStudent = self.student
+		tempStudent.isMatched = False
+		tempStudent.mentor = None
+		self.student = None
+		self.hasStudent = False
 		
+	def match(self, student):
+		self.student = student
+                self.hasStudent = True
+                self.matchScore = student.matches(self.name)
+                student.mentor = self
+                student.isMatched = True
