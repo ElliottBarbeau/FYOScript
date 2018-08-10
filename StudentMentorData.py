0863b03a9f5137df459eb407b6e7e2a727a4f00a
Declan McMullen <decomcmullen@gmail.com>
2018-08-10 15:49:10 -0400

    Adding variables

--- StudentMentorData.py
+++ StudentMentorData.py
@@ -1,1 +1,11 @@
+stringmentor = []
+stringstudent = []
 
+namelist = []
+
+students = {}
+mentors = {}
+
+"""
+This will be updated to initialize the dictionaries and lists so that the rest of the code can use it.
+"""
